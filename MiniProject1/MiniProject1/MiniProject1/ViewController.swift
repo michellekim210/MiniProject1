b75f3740cfb06d1e492b84d0a2645998b8f90f45
michellekim210 <69254574+michellekim210@users.noreply.github.com>
2020-08-05 23:34:47 -0400

    Add files via upload

---
+++ MiniProject1/MiniProject1/MiniProject1/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by Michelle Kim on 8/5/20.
+//  Copyright © 2020 Michelle Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var fact1: UILabel!
+    @IBOutlet weak var fact2: UILabel!
+    @IBOutlet weak var fact3: UILabel!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+   
+    }
+
+    @IBAction func musicButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func popButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func noButton(_ sender: UIButton) {
+    }
+    
+}
+
