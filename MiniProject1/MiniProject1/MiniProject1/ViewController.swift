bbb70e01091e533218d13e45052ddadad3d647ec
michellekim210 <69254574+michellekim210@users.noreply.github.com>
2020-08-06 10:51:29 -0400

    (updated)

--- MiniProject1/MiniProject1/MiniProject1/ViewController.swift
+++ MiniProject1/MiniProject1/MiniProject1/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
     @IBOutlet weak var fact1: UILabel!
     @IBOutlet weak var fact2: UILabel!
     @IBOutlet weak var fact3: UILabel!
@@ -19,16 +20,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-   
+        fact1.text = ""
+        fact2.text = ""
+        fact3.text = ""
     }
 
     @IBAction func musicButton(_ sender: UIButton) {
+        fact1.text = "Instruments I play/used to play: piano, flute, cello, saxophone, guitar"
     }
     
     @IBAction func popButton(_ sender: UIButton) {
+        fact2.text = "Goldfish is my favorite snack"
     }
     
     @IBAction func noButton(_ sender: UIButton) {
+        fact3.text = "I hate the sound of velcro"
     }
     
 }
